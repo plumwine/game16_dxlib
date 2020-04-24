192ff005603f5d0dcec518e315afab898832f2ce
shinjou_kun <shinjoubou03@gmail.com>
2020-04-24 16:27:26 +0900

    新たな時代の幕開け

--- game16_lib/main.cpp
+++ game16_lib/main.cpp
@@ -1,5 +1,7 @@
 #include "DxLib.h"
 #include <crtdbg.h>
+#include"Player.h"
+#include"Enemy.h"
 
 
 // プログラムは WinMain から始まります
@@ -15,20 +17,24 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	ChangeWindowMode(true);
 	SetMainWindowText("game16");
 
+	//宣言（仮）
+	
 	//	画面サイズを設定
-	SetGraphMode(840, 640, 16);
+	SetGraphMode(800, 600, 16);
 	if (DxLib_Init() == -1)	// ＤＸライブラリ初期化処理
 	{
 		return -1;				// エラーが起きたら直ちに終了
 	}
 
+	
 	//	描画先を指定
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	//	画面背景の色を設定する
-	SetBackgroundColor(100, 100, 2);
-	WaitKey();              // キー入力待ち
+	SetBackgroundColor(0, 255, 100);
 
+
+	
 	DxLib_End();            // ＤＸライブラリ使用の終了処理
 
 	return 0;               // ソフトの終了 

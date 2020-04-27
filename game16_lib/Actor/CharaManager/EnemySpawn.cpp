23eaad776b80178c6728a588f4957f309a1b8c0c
syake <->
2020-04-27 19:21:33 +0900

    敵生成クラスの追加（今はランダムで即席用）

---
+++ game16_lib/Actor/CharaManager/EnemySpawn.cpp
@@ -0,0 +1,45 @@
+#include "EnemySpawn.h"
+#include <random>
+#include "../Enemies/Enemy.h"
+
+
+EnemySpawn::EnemySpawn(CharactorManager& charactorManager):
+	mCharactorManager(&charactorManager),
+	mTimer(new Timer())
+{
+}
+
+
+EnemySpawn::~EnemySpawn()
+{
+	delete mTimer;
+}
+
+void EnemySpawn::initialize()
+{
+	mTimer->initialize();  //タイマーを初期化
+}
+
+void EnemySpawn::update(float deltaTime)
+{
+	mTimer->update(deltaTime);
+}
+
+void EnemySpawn::spawn()
+{
+	//5秒おきに敵が生成される
+	if (mTimer->timerSet(5))
+	{
+		//疑似乱数に初期シードを与える
+		std::mt19937 mt{ std::random_device{}() };
+		//ディストリビューションを使って範囲指定（整数）
+		std::uniform_int_distribution<int> spawnNum(1, 10);
+
+		for (int i = 0; i < spawnNum(mt); i++)
+		{
+			mCharactorManager->add(new Enemy(Vector2((i + 1) * 100, 50)));
+		}
+
+	}
+
+}
